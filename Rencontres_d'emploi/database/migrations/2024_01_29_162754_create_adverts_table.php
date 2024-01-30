154bf45a6c751ce705803fc3c86777c259e34f51
yfakihi <fakihiyassin01@gmail.com>
2024-01-30 11:15:55 +0100

    creete migration and added install packege breeze for config Auth

--- Rencontres_d'emploi/database/migrations/2024_01_29_162754_create_adverts_table.php
+++ Rencontres_d'emploi/database/migrations/2024_01_29_162754_create_adverts_table.php
@@ -11,14 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('adverts', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->enum('role',['lerner','admin'])->default('lerner');
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('title');
+            $table->text('content');
+            $table->unsignedBigInteger('partner_id');
+            $table->foreign('partner_id')->references('id')->on('partners')->onDelete("cascade");
+            
             $table->timestamps();
         });
     }
@@ -28,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('adverts');
     }
 };
